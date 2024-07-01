caccc4e2c89812f376bb1dd6e5ee7981bc1b1721
Gabriel Andrade <gabo43211@gmail.com>
2024-07-01 17:54:17 -0600

    actualizacion pagina

--- preparate.php
+++ preparate.php
@@ -5,65 +5,49 @@
 <header class="header">
   <div class="container">
     <h1 class="header__titulo">PREPÁRATE PARA<br>EL CONGRESO</h1>
+    <img src="build/img/linea.png" alt="">
   </div>
 </header>
 
 <div class="container my-5">
 
-  <div class="col">
-    <h2 class="subtituloPMin">1. Conoce acerca del</h2>
-  </div>
-
   <div class="row">
     <div class="col-md-6 col-sm-12">
+      <h2 class="subtituloPMin">1. Conoce acerca del congreso</h2>
       <ul>
-        <li>Antes de poder discutir con tu jefe tu posible participación al Congreso, es importante que conozcas a fondo la estructura del mismo, fechas, horarios, logística, tipos de conferencias y conferencistas y precios.</li>
+        <li>Antes de poder discutir con tu jefe tu posible participación al Congreso, es importante que <b>conozcas a fondo la estructura</b> del mismo, fechas, horarios, logística, tipos de conferencias y conferencistas y precios.</li>
         <br>
-        <li>A través de conocer toda esta información establece metas a corto y largo plazo que te ayuden a cumplir mejor tu trabajo. Estas metas deben de ser apoyadas por la idea de asistir al evento.</li>
+        <li>A través de conocer toda esta información <b>establece metas</b> a corto y largo plazo que te <b>ayuden a cumplir mejor tu trabajo.</b> Estas metas deben de ser apoyadas por la idea de asistir al evento.</li>
       </ul>
-      <div class="container mt-5">
-        <div class="col">
-          <!-- <a href="registro_sesiones.php" class="btn btn__primary Disable">VER PONENTES</a> -->
-        </div>
-      </div>
     </div>
     <div class="col-md-6 col-sm-12 text-center" data-aos="fade-left">
-      <img src="./build/img/Recursopreventa.png" alt="" class="convocatoria__perfil">
+      <img src="./build/img/Recursopreventa.png" alt="" class="convocatoria__perfil" style="width: 60%; height: auto;">
     </div>
   </div>
 </div>
 
 <div class="container-fluid primaryBg mt-5 mb-5">
   <div class="container py-5">
-    <div class="col-md-6 col-sm-12" data-aos="fade-left">
-      <h2 class="header__titulo">2. Organiza tu agenda de conferencias</h2>
-    </div>
     <div class="row">
       <div class="col-md-6 col-sm-12">
+        <h2 class="subtituloBMin">2. Organiza tu agenda de conferencias</h2>
         <ul>
           <li>Alinea tus objetivos laborales y los de tu organización con los objetivos de las conferencias y talleres a los que te gustaría asistir. La mejor forma de justificar tu asistencia al Congreso es que lo que vas a aprender te ayude a poder hacer mejor tu trabajo.</li>
           <br>
           <li>Establece una agenda de contactos y planea un posible acercamiento con algún conferencista con el cual le llegue interesar a tu organización hacer relación.</li>
         </ul>
-        <div class="container mt-5">
-          <div class="col">
-            <!-- <a href="registro_sesiones.php" class="btn btn__primary Disable">VER PROGRAMA</a> -->
-          </div>
-        </div>
       </div>
       <div class="col-md-6 col-sm-12 text-center" data-aos="fade-left">
-        <img src="./build/img/Recurso1preventa.png" alt="" class="convocatoria__perfil">
+        <img src="./build/img/Recurso1preventa.png" alt="" class="convocatoria__perfil" style="width: 40%; height: auto;">
       </div>
     </div>
   </div>
 </div>
 
 <div class="container my-5">
-  <div class="col">
-    <h2 class="subtituloPMin">3. Conoce la lista de razones</h2>
-  </div>
   <div class="row">
     <div class="col-md-6 col-sm-12">
+      <h2 class="subtituloPMin">3. Conoce la lista de razones</h2>
       <ul>
         <li>Aprender de los mejores conferencistas y talleristas del mundo sobre parques y espacios públicos.</li>
         <br>
@@ -83,22 +67,19 @@
       </ul>
     </div>
     <div class="col-md-6 col-sm-12 text-center" data-aos="fade-left">
-      <img src="./build/img/Recurso4preventa.png" alt="" class="convocatoria__perfil">
+      <img src="./build/img/Recurso4preventa.png" alt="" class="convocatoria__perfil" style="width: 65%; height: auto;">
     </div>
   </div>
 </div>
 
 <div class="container-fluid primaryBg mt-5 mb-5">
   <div class="container py-5">
-    <div class="col-md-6 col-sm-12" data-aos="fade-left">
-      <h2 class="header__titulo">4. Carta para justificar tu asistencia</h2>
-    </div>
     <div class="row">
       <div class="col-md-6 col-sm-12">
+        <h2 class="subtituloBMin">4. Carta para justificar tu asistencia</h2>
         <ul>
-          <li>Descarga para justificar.</li>
+          <li>Descarga las cartas.</li>
           <br>
-          
         </ul>
         <div class="container mt-5">
           <div class="col">
@@ -112,7 +93,7 @@
         </div>
       </div>
       <div class="col-md-6 col-sm-12 text-center" data-aos="fade-left">
-        <img src="./build/img/Recurso2preventa.png" alt="" class="convocatoria__perfil">
+        <img src="./build/img/Recurso2preventa.png" alt="" class="convocatoria__perfil" style="width: 65%; height: auto;">
       </div>
     </div>
   </div>
